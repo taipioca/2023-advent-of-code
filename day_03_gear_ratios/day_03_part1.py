05500d4e15e972e0029cd8894e44f61b8a022626
taipioca <leanntai94@gmail.com>
2023-12-03 13:20:15 -0500

    wrong but almost works

--- day_03_gear_ratios/day_03_part1.py
+++ day_03_gear_ratios/day_03_part1.py
@@ -12,20 +12,22 @@ def gear(file):
         schematic = [line for line in puzzle_input]
     split = []
 
+    # cretae and clean up the new 2d array
     for line in schematic:
         split.append(list(line))
     for line in range(len(split)-1):
         split[line].insert(0, '.')
         split[line].pop(len(split[line])-1)
         split[line].append('.')
-
     split.insert(0, ['.']*(len(split[1])-1))
     split.append(['.']*(len(split[1])-1))
     part_number = []
 
+    # store current number and where on the line the number is
+    # figure out if its a part number then add the number to part number list
     for line in range(len(split)):
-        num = ''
-        num_indices = []
+        num = '' 
+        num_indices = [] 
         for i in range(len(split[line])):
             if split[line][i].isdigit():
                 num_indices.append(i)
